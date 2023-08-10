7ddc7536a436a73f572b7bce8668f70fbaa0fc35
Hwan0518 <qip0518@naver.com>
2023-08-10 21:54:45 +0900

    init: 프로젝트 초기 생성
    ssg point app 프로젝트 초기 생성

---
+++ src/main/java/ssgssak/ssgpointuser/SsgpointUserApplication.java
@@ -0,0 +1,13 @@
+package ssgssak.ssgpointuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgpointUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgpointUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssgssak/ssgpointuser/SsgpointUserApplicationTests.java
@@ -0,0 +1,13 @@
+package ssgssak.ssgpointuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgpointUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
