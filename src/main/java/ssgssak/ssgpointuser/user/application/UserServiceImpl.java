ed2c7a65f9dbfb002f3f32a21dc876969624e5c1
Hwan0518 <qip0518@naver.com>
2023-08-16 15:08:35 +0900

    feat: 회원정보 수정 기능 개발
    userUpdateInfo - Vo,Dto,Controller,Service,Converter,Repository 생성 및 기능추가
    회원정보 수정에는 address와 email만 수정할 수 있음

---
+++ src/main/java/ssgssak/ssgpointuser/user/application/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package ssgssak.ssgpointuser.user.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ssgssak.ssgpointuser.user.dto.UserUpdateInfoDto;
+import ssgssak.ssgpointuser.user.entity.User;
+import ssgssak.ssgpointuser.user.infrastructure.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+    private final UserConverter userConverter;
+
+    @Override
+    public void updateUserInfo(UserUpdateInfoDto updateInfoDto) {
+        User updateUser = userConverter.updateInfoDtoToUser(updateInfoDto);
+        userRepository.save(updateUser);
+    }
+
+    /**
+     * 유저를 UUID로 검색
+     */
+    public User getUserByUUID(String userUUID) {
+        User user = userRepository.findUserByUserUUID(userUUID)
+                .orElseThrow(()->new NoSuchElementException());
+        return user;
+    }
+
+}
+
