1c9a85e6eda2880960bb47a690d0d681ff59d17e
Hwan0518 <qip0518@naver.com>
2023-08-20 13:28:54 +0900

    feat: Point 엔티티, PointType Enum 생성
    Point 도메인 - entity와 enum타입만 생성

---
+++ src/main/java/ssgssak/ssgpointuser/domain/point/entity/PointType.java
@@ -0,0 +1,14 @@
+package ssgssak.ssgpointuser.domain.point.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum PointType {
+    STORE,
+    GIFT,
+    EXCHANGE,
+    ATTENDANCE,
+    ROULETTE,
+    PARTNER,
+    EVENT
+}
