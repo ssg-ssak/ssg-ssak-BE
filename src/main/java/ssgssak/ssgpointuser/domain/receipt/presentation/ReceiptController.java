660e3140109d910cbf4e663221f74baddc796598
Hwan0518 <qip0518@naver.com>
2023-09-04 16:11:44 +0900

    bug: controller에서 mapping 생략된 부분 추가

--- src/main/java/ssgssak/ssgpointuser/domain/receipt/presentation/ReceiptController.java
+++ src/main/java/ssgssak/ssgpointuser/domain/receipt/presentation/ReceiptController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ssgssak.ssgpointuser.domain.receipt.application.ReceiptServiceImpl;
@@ -29,11 +31,13 @@ public class ReceiptController {
      */
 
     // 1. 영수증 저장
+    @PostMapping("")
     public void saveReceipt(ReceiptSaveInVo inVo) {
         receiptService.saveReceipt(modelMapper.map(inVo, ReceiptSaveRequestDto.class));
     }
 
     // 2. 영수증 id로, 영수증 조회
+    @GetMapping("")
     public ResponseEntity<ReceiptGetOutVo> getReceipt(ReceiptGetInVo inVo) {
         ReceiptGetResponseDto responseDto = receiptService.getReceipt(modelMapper.map(inVo, ReceiptGetRequestDto.class));
         ReceiptGetOutVo outVo = modelMapper.map(responseDto, ReceiptGetOutVo.class);
