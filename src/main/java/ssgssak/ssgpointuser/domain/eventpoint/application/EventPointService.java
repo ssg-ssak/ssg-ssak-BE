317a436abf6cb276fac3197ff31deacc0134cb72
Hwan0518 <qip0518@naver.com>
2023-09-05 11:58:15 +0900

    feat: 어제자 출석체크 포인트 조회 기능 생성
    eventpoint 도메인 - 포인트 id를 받아서, 어제자 attendancePoint 조회기능 생성

--- src/main/java/ssgssak/ssgpointuser/domain/eventpoint/application/EventPointService.java
+++ src/main/java/ssgssak/ssgpointuser/domain/eventpoint/application/EventPointService.java
@@ -1,6 +1,7 @@
 package ssgssak.ssgpointuser.domain.eventpoint.application;
 
 import ssgssak.ssgpointuser.domain.eventpoint.dto.AttendancePointAddDto;
+import ssgssak.ssgpointuser.domain.eventpoint.dto.AttendancePointGetResponseDto;
 import ssgssak.ssgpointuser.domain.eventpoint.dto.RoulettePointAddDto;
 
 import java.time.LocalDateTime;
@@ -10,12 +11,16 @@ public interface EventPointService {
      * 이벤트 포인트 적립
      * 1. 출석체크 포인트 적립
      * 2. 룰렛 포인트 적립
+     * 3. 어제자 출석체크 포인트 조회
      */
 
 
     // 1. 출석체크 포인트 적립
-    void addAttendancePoint(AttendancePointAddDto addDto, String uuid);
+    void addAttendancePoint(AttendancePointAddDto addDto);
 
     // 2. 룰렛 포인트 적립
-    void addRoulettePoint(RoulettePointAddDto addDto, String uuid);
+    void addRoulettePoint(RoulettePointAddDto addDto);
+
+    // 3. 어제자 출석체크 포인트 조회
+    AttendancePointGetResponseDto getYesterdayAttendancePoint(Long pointId);
 }
