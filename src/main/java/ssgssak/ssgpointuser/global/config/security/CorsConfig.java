7c38428c8a6ec004a2feed20caee4e54905dad33
Hwan0518 <qip0518@naver.com>
2023-09-13 15:50:27 +0900

    [fixed] cors register

--- src/main/java/ssgssak/ssgpointuser/global/config/security/CorsConfig.java
+++ src/main/java/ssgssak/ssgpointuser/global/config/security/CorsConfig.java
@@ -3,18 +3,22 @@ package ssgssak.ssgpointuser.global.config.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://10.10.10.186:3000", "http://localhost:3000", "https://ssg-ssak-gdu36kh90-chanseop.vercel.app/")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowCredentials(true)
-                .maxAge(3000);
+                .allowCredentials(true).maxAge(3600)
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+
     }
 }
