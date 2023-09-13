e46ec2e5ce603406934eef9ff5c6712070ff09f6
Hwan0518 <qip0518@naver.com>
2023-09-13 15:38:58 +0900

    test: cors에러 테스트2

--- src/main/java/ssgssak/ssgpointuser/global/config/security/CorsConfig.java
+++ src/main/java/ssgssak/ssgpointuser/global/config/security/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-//                .allowedOrigins("http://10.10.10.186:3000", "http://localhost:3000", "https://ssg-ssak-gdu36kh90-chanseop.vercel.app/")
+                .allowedOrigins("http://10.10.10.186:3000", "http://localhost:3000", "https://ssg-ssak-gdu36kh90-chanseop.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3000);
