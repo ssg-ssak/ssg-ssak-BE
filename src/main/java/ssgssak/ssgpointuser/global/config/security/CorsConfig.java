d4cd3c07d8bb4615e0cb9bba4372061b6a646959
Hwan0518 <qip0518@naver.com>
2023-09-18 19:28:42 +0900

    fix: Cors 필요없는 어노테이션 제거

--- src/main/java/ssgssak/ssgpointuser/global/config/security/CorsConfig.java
+++ src/main/java/ssgssak/ssgpointuser/global/config/security/CorsConfig.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc // 여기에는 필요없음 -> 웹 요청 및 응답을 처리하는데 필요 ex) 컨트롤러, 뷰 리졸버, 핸들러 매핑, 인터셉터 등을 활성화 함
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowCredentials(true).maxAge(3600)
+                .allowCredentials(true).maxAge(3600) // maxAge는 preflight 정보를 캐싱해두는 기간이다. 이 기간동안은 preflight request를 보내지 않는다.
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
